78bdc279aa6735f1641df43787bc8c67fdf157b5
DiChris2901 <daruedagu@gmail.com>
2025-07-16 12:48:05 -0500

    init-structure: Vite + React base creada

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
-import { CustomThemeProvider } from "./contexts/ThemeContext.jsx"; // 👈 nuevo
-import { BrowserRouter } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CustomThemeProvider>
-        <BrowserRouter>
-          <CssBaseline />
-          <App />
-        </BrowserRouter>
-      </CustomThemeProvider>
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
